6e827ad9e06b2b0ccb066c535dec8b10d7eba338
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2026-04-17 19:35:22 +0200

    feat: User getbyid endpoint

---
+++ src/ToDo.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ToDo.Domain.Interfaces;
+
+namespace ToDo.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository, IPasswordService passwordService)
+        {
+            _userRepository = userRepository;
+        }
+
+        // GET: api/user/me
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMyProfile()
+        {
+            var userId = GetCurrentUserId();
+
+            var user = await _userRepository.GetByIdAsync(userId);
+
+            if (user == null)
+                return NotFound(new { message = "User not found" });
+
+            return Ok(new
+            {
+                user.Id,
+                user.Email,
+                user.Username,
+                user.CreatedAt
+            });
+        }
+
+        private int GetCurrentUserId()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return int.Parse(userIdClaim!);
+        }
+    }
+}
