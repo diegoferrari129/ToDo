d272f55b7629177a7d5576bab80ce16480adba7e
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2026-04-26 22:12:53 +0200

    upd: enanched password validation

--- src/ToDo.Application/DTOs/AuthDtos/UserRegisterRequest.cs
+++ src/ToDo.Application/DTOs/AuthDtos/UserRegisterRequest.cs
@@ -18,7 +18,7 @@ namespace ToDo.Application.DTOs.AuthDtos
         public string Password { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Confirm Password is required")]
-        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")] 
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
