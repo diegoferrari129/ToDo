137e90b26bdf14de07b3a4950fa5924cac5b49e8
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2026-04-26 22:30:39 +0200

    Merge ReadyForProduction to master: enanched register password validation

--- src/ToDo.Application/DTOs/AuthDtos/UserRegisterRequest.cs
+++ src/ToDo.Application/DTOs/AuthDtos/UserRegisterRequest.cs
@@ -16,7 +16,7 @@ namespace ToDo.Application.DTOs.AuthDtos
         [Required(ErrorMessage = "Password is required")]
         [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
         public string Password { get; set; } = string.Empty;
-
+         
         [Required(ErrorMessage = "Confirm Password is required")]
         [Compare("Password", ErrorMessage = "Passwords do not match")] 
         public string ConfirmPassword { get; set; } = string.Empty;
